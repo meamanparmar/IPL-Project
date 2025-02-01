61878e8825761d92dd1bcc079c6eab6d2a73ed04
username <wecp-dummy@gmail.com>
2025-02-01 11:17:45 +0000

    wecp initial commit

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -1,5 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  successMessage: string = '';
+  errorMessage: string = '';
 
-export class LoginComponent  {
-  
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*\d).+$/)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username } = this.loginForm.value;
+      if (username === 'errorUser') {
+        this.simulateBackendLoginError();
+      } else {
+        this.successMessage = 'Login successful!';
+        this.errorMessage = '';
+      }
+    } else {
+      this.successMessage = '';
+      this.errorMessage = 'Please fill out all required fields correctly.';
+    }
+  }
+
+  simulateBackendLoginError() {
+    this.successMessage = '';
+    this.errorMessage = 'Invalid username or password.';
+  }
 }
